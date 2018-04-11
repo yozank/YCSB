ff729b19149ab22408bc613b8a99efa62890e1c0
Ozan Kılıç <ozan@hazelcast.com>
2018-04-11 18:39:31 +0300

    ..

--- hazelcast/src/main/java/com/yahoo/ycsb/db/HazelcastDBClient.java
+++ hazelcast/src/main/java/com/yahoo/ycsb/db/HazelcastDBClient.java
@@ -21,8 +21,6 @@ import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.config.XmlClientConfigBuilder;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
-import com.hazelcast.query.PagingPredicate;
 import com.hazelcast.query.Predicate;
 import com.hazelcast.query.Predicates;
 import com.yahoo.ycsb.ByteIterator;
